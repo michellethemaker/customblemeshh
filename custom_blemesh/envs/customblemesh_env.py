c47bc81410118b9ab596b74cd7bc1b92e3016581
michellethemaker <83628697+michellethemaker@users.noreply.github.com>
2021-10-21 01:31:16 +0800

    Update customblemesh_env.py

--- custom_blemesh/envs/customblemesh_env.py
+++ custom_blemesh/envs/customblemesh_env.py
@@ -82,81 +82,79 @@ class customblemeshEnv(gym.Env):
 
 metadata = {"render.modes": ["human", "ansi"]}
 
-    def __init__(self, desc=None, map_name="4x4", is_slippery=True):
+   def __init__(self, desc=None, map_name="4x4", is_slippery=True):
         if desc is None and map_name is None:
             desc = generate_random_map()
         elif desc is None:
             desc = MAPS[map_name]
-    self.desc = desc = np.asarray(desc, dtype="c")
-    self.nrow, self.ncol = nrow, ncol = desc.shape
-    self.reward_range = (0, 1)
+        self.desc = desc = np.asarray(desc, dtype="c")
+        self.nrow, self.ncol = nrow, ncol = desc.shape
+        self.reward_range = (0, 1)
 
-    nA = 4
-    nS = nrow * ncol
+        nA = 4
+        nS = nrow * ncol
 
-    isd = np.array(desc == b"S").astype("float64").ravel()
-    isd /= isd.sum()
+        isd = np.array(desc == b"S").astype("float64").ravel()
+        isd /= isd.sum()
 
-    P = {s: {a: [] for a in range(nA)} for s in range(nS)}
+        P = {s: {a: [] for a in range(nA)} for s in range(nS)}
 
-    def to_s(row, col):
-        return row * ncol + col
+        def to_s(row, col):
+            return row * ncol + col
 
-    def inc(row, col, a):
-        if a == LEFT:
-            col = max(col - 1, 0)
-        elif a == DOWN:
-            row = min(row + 1, nrow - 1)
-        elif a == RIGHT:
-            col = min(col + 1, ncol - 1)
-        elif a == UP:
-            row = max(row - 1, 0)
-        return (row, col)
+        def inc(row, col, a):
+            if a == LEFT:
+                col = max(col - 1, 0)
+            elif a == DOWN:
+                row = min(row + 1, nrow - 1)
+            elif a == RIGHT:
+                col = min(col + 1, ncol - 1)
+            elif a == UP:
+                row = max(row - 1, 0)
+            return (row, col)
 
-    def update_probability_matrix(row, col, action):
-        newrow, newcol = inc(row, col, action)
-        newstate = to_s(newrow, newcol)
-        newletter = desc[newrow, newcol]
-        done = bytes(newletter) in b"GH"
-        reward = float(newletter == b"G")
-        return newstate, reward, done
+        def update_probability_matrix(row, col, action):
+            newrow, newcol = inc(row, col, action)
+            newstate = to_s(newrow, newcol)
+            newletter = desc[newrow, newcol]
+            done = bytes(newletter) in b"GH"
+            reward = float(newletter == b"G")
+            return newstate, reward, done
 
-    for row in range(nrow):
-        for col in range(ncol):
-            s = to_s(row, col)
-            for a in range(4):
-                li = P[s][a]
-                letter = desc[row, col]
-                if letter in b"GH":
-                    li.append((1.0, s, 0, True))
-                else:
-                    if is_slippery:
-                        for b in [(a - 1) % 4, a, (a + 1) % 4]:
-                            li.append(
-                                (1.0 / 3.0, *update_probability_matrix(row, col, b))
-                            )
+        for row in range(nrow):
+            for col in range(ncol):
+                s = to_s(row, col)
+                for a in range(4):
+                    li = P[s][a]
+                    letter = desc[row, col]
+                    if letter in b"GH":
+                        li.append((1.0, s, 0, True))
                     else:
-                        li.append((1.0, *update_probability_matrix(row, col, a)))
+                        if is_slippery:
+                            for b in [(a - 1) % 4, a, (a + 1) % 4]:
+                                li.append(
+                                    (1.0 / 3.0, *update_probability_matrix(row, col, b))
+                                )
+                        else:
+                            li.append((1.0, *update_probability_matrix(row, col, a)))
 
-    super(FrozenLakeEnv, self).__init__(nS, nA, P, isd)
+        super(FrozenLakeEnv, self).__init__(nS, nA, P, isd)
 
     def render(self, mode="human"):
-    outfile = StringIO() if mode == "ansi" else sys.stdout
+        outfile = StringIO() if mode == "ansi" else sys.stdout
 
-    row, col = self.s // self.ncol, self.s % self.ncol
-    desc = self.desc.tolist()
-    desc = [[c.decode("utf-8") for c in line] for line in desc]
-    desc[row][col] = utils.colorize(desc[row][col], "red", highlight=True)
-    if self.lastaction is not None:
-        outfile.write(
-            "  ({})\n".format(["Left", "Down", "Right", "Up"][self.lastaction])
-        )
-    else:
-        outfile.write("\n")
-    outfile.write("\n".join("".join(line) for line in desc) + "\n")
+        row, col = self.s // self.ncol, self.s % self.ncol
+        desc = self.desc.tolist()
+        desc = [[c.decode("utf-8") for c in line] for line in desc]
+        desc[row][col] = utils.colorize(desc[row][col], "red", highlight=True)
+        if self.lastaction is not None:
+            outfile.write(
+                "  ({})\n".format(["Left", "Down", "Right", "Up"][self.lastaction])
+            )
+        else:
+            outfile.write("\n")
+        outfile.write("\n".join("".join(line) for line in desc) + "\n")
 
-    if mode != "human":
-        with closing(outfile):
-            return outfile.getvalue()
-                
-                
+        if mode != "human":
+            with closing(outfile):
+                return outfile.getvalue()
